413db36c8a2b61157b68218d1af701650b0cef00
Yana-Romanenko <yanaromanenko@ukr.net>
2024-01-08 17:13:38 +0200

    updated file

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -13,10 +13,9 @@ const hoursTimer = document.querySelector('[data-hours]');
 const minutesTimer = document.querySelector('[data-minutes]');
 const secondsTimer = document.querySelector('[data-seconds]');
 
-// оголоси поза межами методу onClose() let змінну userSelectedDate
+
 let userSelectedDate = '';
 
-//об'єкт параметрів бібліотеки flatpickr
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -24,7 +23,6 @@ const options = {
   minuteIncrement: 1,
 
   onClose(selectedDates) {
-    // перевірка дати на майбутність
     if (selectedDates[0] < new Date()) {
       iziToast.error({
         title: 'Error',
@@ -45,17 +43,14 @@ const options = {
 };
 
 //initial flatpickr
-//створюємо екземпляр бібліотеки
 const datePicker = flatpickr(input, options);
 
-//додаємо слухач на input
 input.addEventListener('focus', () => {
   datePicker.config.defaultDate = new Date();
 });
 
 let intervalId;
 
-// Start the countdown
 buttonStart.addEventListener('click', () => {
   buttonStart.disabled = true;
 
@@ -63,7 +58,7 @@ buttonStart.addEventListener('click', () => {
   const selectedTime = userSelectedDate.getTime();
   let countdownInterval = selectedTime - currentTime;
 
-  //перша ініціалізація відображення таймера
+ 
   const time = convertMs(countdownInterval);
   updateTimerDisplay(time);
 
@@ -77,11 +72,9 @@ buttonStart.addEventListener('click', () => {
     }
   }, 1000);
 
-  // Очищення поля вводу
   input.value = '';
 });
 
-// оновлення відображення таймера
 function updateTimerDisplay(time) {
   daysTimer.textContent = addLeadingZero(time.days);
   hoursTimer.textContent = addLeadingZero(time.hours);
@@ -90,19 +83,14 @@ function updateTimerDisplay(time) {
 }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
@@ -115,6 +103,3 @@ function convertMs(ms) {
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
-// style
-// const label = document.querySelector('.label');
-// label.style = 'text-transform: uppercase;';
